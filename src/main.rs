bd36129832ac7f46fdaa9f9bc9044c43a30ed16e
Cristiano <up202108813@g.uporto.pt>
2023-01-24 14:25:23 +0000

    Already printing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+extern crate core;
+
 use crate::table::Table;
 
 mod table;
 
 fn main() {
     let mut t = Table {
-        style: 1,
-        row_id: true,
+        style: 3,
         auto_format: true,
         headers: vec!["name".to_owned(), "age".to_owned(), "email".to_owned(), "random".to_owned()],
         rows: Vec::new(),
         word_len: Vec::new(),
     };
 
-    t.insert_row(vec!["Cristiano".to_owned(), "20".to_owned(), "email".to_owned(), "random".to_owned()]);
+    t.insert_row(vec!["Cristiano".to_owned(), "20".to_owned(), "cristianorocha170@gmail.com".to_owned(), "fffffff".to_owned()]);
+    t.insert_row(vec!["Rocha".to_owned(), "20".to_owned(), "rocha170@gmail.com".to_owned(), "dasdasdadgdgdgd".to_owned()]);
+    t.insert_row(vec!["Pedro".to_owned(), "20".to_owned(), "rocha170@gmail.com".to_owned(), "dkjanskdnaksd".to_owned()]);
+
     println!("{:?}", t.get_rows());
-    t.normal_print();
+    t.print();
 }
--- src/table.rs
+++ src/table.rs
@@ -3,7 +3,6 @@ use std::io::repeat;
 
 pub struct Table {
     pub style: i32,
-    pub row_id: bool,
     pub auto_format: bool,
     pub headers: Vec<String>,
     pub rows: Vec<Vec<String>>,
@@ -15,89 +14,115 @@ impl Table {
     /// Desired style for the table format:
     /// 1 -> +---------+---------+---------+
     /// 2 -> special characters
-    pub fn get_style(&self) -> i32 {self.style}
-    pub fn get_row_id(&self) -> bool {self.row_id}
-    pub fn get_format(&self) -> bool {self.auto_format}
-    pub fn get_headers(&self) -> Vec<String> {self.headers.clone()}
-    pub fn get_rows(&self) -> Vec<Vec<String>> {self.rows.clone()}
+    pub fn get_style(&self) -> i32 { self.style }
+    pub fn get_format(&self) -> bool { self.auto_format }
+    pub fn get_headers(&self) -> Vec<String> { self.headers.clone() }
+    pub fn get_rows(&self) -> Vec<Vec<String>> { self.rows.clone() }
 
 
     pub fn insert_row(&mut self, row: Vec<String>) {
-        if self.word_len.is_empty() {self.word_len = vec![0; self.headers.len()]}
+        if self.word_len.is_empty() { self.word_len = vec![0; self.headers.len()] }
 
         for i in 0..row.len() {
-            if row[i].len() > self.word_len[i] {self.word_len[i] = row[i].len();}
-            if self.headers.len() > self.word_len[i] {self.word_len[i] = self.headers[i].len();}
+            if row[i].len() > self.word_len[i] { self.word_len[i] = row[i].len(); }
+            if self.headers.len() > self.word_len[i] { self.word_len[i] = self.headers[i].len(); }
         }
         self.rows.push(row);
     }
 
-    pub fn normal_print(&self) {
-        let top_left = "┏";
-        let top_right = "┓";
-        let bottom_left = "┗";
-        let bottom_right = "┛";
+    pub fn print(&self) -> String{
+        let mut top_left = "+";
+        let mut top_right = "+";
+        let mut bottom_left = "+";
+        let mut bottom_right = "+";
+        let mut top_intersect = "+";
+        let mut bottom_intersect = "+";
+        let mut right_separator = "+";
+        let mut left_separator = "+";
+        let mut separator = "+";
+        let mut line = "-";
+        let mut col = "|";
 
-        let top_intersect = "┳";
-        let bottom_intersect = "┻";
-        let right_separator = "┫";
-        let left_separator = "┣";
-        let separator = "╋";
-        let line = "━";
-        let col = "┃";
+        match self.style {
+            1 => {}
+            2 => {
+                top_left = "┏";
+                top_right = "┓";
+                bottom_left = "┗";
+                bottom_right = "┛";
+
+                top_intersect = "┳";
+                bottom_intersect = "┻";
+                right_separator = "┫";
+                left_separator = "┣";
+                separator = "╋";
+                line = "━";
+                col = "┃";
+            }
+            3 => {
+                top_left = "╔";
+                top_right = "╗";
+                bottom_left = "╚";
+                bottom_right = "╝";
+
+                top_intersect = "╦";
+                bottom_intersect = "╩";
+                right_separator = "╣";
+                left_separator = "╠";
+                separator = "╬";
+                line = "═";
+                col = "║";
+            }
+            _ => { panic!("Error: Style not accepted") }
+        }
+
 
         let n_cols = self.headers.len();
         let n_rows = self.rows.len();
-
-        // +--------+--------+--------+
-        // |name    |age     |email   |
-        // +--------+--------+--------+
-
         let mut top_line: String = top_left.to_string();
+        let mut end_line: String = bottom_left.to_string();
         let mut header: String = String::new();
         let mut normal_line: String = String::new();
+        let mut res: String = String::new();
+
+
         for i in 0..n_cols {
             if i == 0 {
-                normal_line += format!("{}{}", left_separator, line.repeat(self.word_len[i]+2)).as_str();
+                normal_line += format!("{}{}", left_separator, line.repeat(self.word_len[i] + 2)).as_str();
+            } else {
+                normal_line += format!("{}{}", separator, line.repeat(self.word_len[i] + 2)).as_str();
             }
-            else {
-                normal_line += format!("{}{}", separator, line.repeat(self.word_len[i]+2)).as_str();
+            if i == n_cols - 1 {
+                top_line += format!("{}{}", line.repeat(self.word_len[i] + 2), top_right).as_str();
+                end_line += format!("{}{}", line.repeat(self.word_len[i] + 2), bottom_right).as_str();
+            } else {
+                top_line += format!("{}{}", line.repeat(self.word_len[i] + 2), top_intersect).as_str();
+                end_line += format!("{}{}", line.repeat(self.word_len[i] + 2), bottom_intersect).as_str();
             }
-            if i == n_cols-1 {
-                top_line += format!("{}{}", line.repeat(self.word_len[i]+2), top_right).as_str();
+            match self.auto_format {
+                true => header += format!("{}{:^width$}", col, self.headers[i], width = self.word_len[i] + 2).as_str(),
+                false => header += format!("{}{}{}", col, self.headers[i], " ".repeat(self.word_len[i] - self.headers[i].len() + 2)).as_str()
             }
-            else {
-                top_line += format!("{}{}", line.repeat(self.word_len[i]+2), top_intersect).as_str();
-            }
-            header += format!("{}{}{}", col, self.headers[i], " ".repeat(self.word_len[i]-self.headers[i].len()+2)).as_str();
         }
         header += col;
         normal_line += right_separator;
+        res += format!("{}{}{}{}{}{}", top_line.as_str(), "\n", header.as_str(), "\n", normal_line.as_str(), "\n").as_str();
 
-
-        println!("{}", top_line);
-        println!("{}", header);
-        println!("{}", normal_line);
-
-
-        //top_line[5] = "a";
-
-
+        for i in 0..n_rows {
+            for j in 0..n_cols {
+                match self.auto_format {
+                    true => { res += format!("{}{:^width$}", col, self.rows[i][j], width = self.word_len[j] + 2).as_str();}
+                    false => { res += format!("{}{}{}", col, self.rows[i][j], " ".repeat(self.word_len[j] - self.rows[i][j].len() + 2)).as_str();}
+                }
+            }
+            res += format!("{col}").as_str();
+            if i != n_rows - 1 {
+                res += format!("\n{normal_line}\n").as_str();
+            } else {
+                res += format!("\n{end_line}\n").as_str();
+            }
+        }
+        println!("{}", res);
+        res
     }
-
-
-    pub fn print(&self) {
-        //match self.style {
-            //1 => normal_print(),
-            //_ => {}
-        //}
-    }
-
-
-
-
-    pub fn preview(&self) {
-
-    }
-
 }
